e80f387c58413098850a6a14c8c36a01fd7b92e9
mohdehtesham <ehteshamidrishi@gmail.com>
2022-11-16 13:30:17 +0530

    BMIdone

---
+++ Practice/BMICalc/app.js
@@ -0,0 +1,17 @@
+let previousBMIs = [];
+let randomNumber = Math.ceil(Math.random() * 100 + 1);
+
+// Main Function
+function checkGuess() {
+  let guess = parseInt(document.getElementById("guessField").value);
+  let height = parseFloat(document.getElementById("heightField").value);
+  document.getElementById("guessField").value=''
+  document.getElementById("heightField").value=''
+  let BMI= guess/Math.pow(height,2)
+  previousBMIs.push(BMI.toFixed(2))
+  const guessesRemaining = document.querySelector(".lastResult").innerHTML=BMI.toFixed(2);
+  const guessSlot = document.querySelector(".guesses").innerHTML=previousBMIs;
+if (previousBMIs.length==10) {
+    previousBMIs.splice(0,1)
+}
+}
