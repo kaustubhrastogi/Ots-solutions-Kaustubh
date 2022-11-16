cfe3171d9890c8a11e498a0367c125425ccd4732
Kaustubh <kaustubhrastogi.44@gmail.com>
2022-11-16 15:45:44 +0530

    B

--- Practice/BMICalc/app.js
+++ Practice/BMICalc/app.js
@@ -3,11 +3,11 @@ let randomNumber = Math.ceil(Math.random() * 100 + 1);
 
 // Main Function
 function checkGuess() {
-  let guess = parseInt(document.getElementById("guessField").value);
+  let weight = parseInt(document.getElementById("guessField").value);
   let height = parseFloat(document.getElementById("heightField").value);
   document.getElementById("guessField").value=''
   document.getElementById("heightField").value=''
-  let BMI= guess/Math.pow(height,2)
+  let BMI= weight/Math.pow(height,2)
   previousBMIs.push(BMI.toFixed(2))
   const guessesRemaining = document.querySelector(".lastResult").innerHTML=BMI.toFixed(2);
   const guessSlot = document.querySelector(".guesses").innerHTML=previousBMIs;
