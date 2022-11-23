1dbbbd3dd87ab272f946bee863b6d9ef3ae5d53d
Kaustubh <kaustubhrastogi.44@gmail.com>
2022-11-23 12:11:42 +0530

    filtering array with similar name

--- Practice/18nov.js
+++ Practice/18nov.js
@@ -14,25 +14,41 @@
 // result = [{name: 'Akshay'}, {name: 'akshay'}, {name:'AMit'}]
 
 
-function onceCallFn(){
-    onceCallFn  = function newFn (){
-        console.log("inner function")
-    }
-    console.log("outer function - call only once")
-}
-onceCallFn()
-onceCallFn()
-onceCallFn()
-onceCallFn()
+// function onceCallFn(){
+//     onceCallFn  = function newFn (){
+//         console.log("inner function")
+//     }
+//     console.log("outer function - call only once")
+// }
+// onceCallFn()
+// onceCallFn()
+// onceCallFn()
+// onceCallFn()
 
-let arr = [[1,2], [2, 4, [5, 6, [2, 4]]], 8, 9]
-let newArr= arr.reduce((acc,curr)=>{
-    return acc.concat(curr)
-},[])
-let NewArr = newArr.reduce((accu,curr)=>{
-    return accu.concat(curr)
-},[])
-let FinalArr = NewArr.reduce((accu,curr)=>{
-    return accu.concat(curr)
-},[])
-console.log(FinalArr)
+// let arr = [[1,2], [2, 4, [5, 6, [2, 4]]], 8, 9]
+// let newArr= arr.reduce((acc,curr)=>{
+//     return acc.concat(curr)
+// },[])
+// let NewArr = newArr.reduce((accu,curr)=>{
+//     return accu.concat(curr)
+// },[])
+// let FinalArr = NewArr.reduce((accu,curr)=>{
+//     return accu.concat(curr)
+// },[])
+// console.log(FinalArr)
+
+let array1 = [{name:'amit'}, {name: 'Akshay'}, {name:'Sakshi'}]
+let array2 = [{name: 'abcd'},{name:'sakshi'}, {name:'kjdshck'}, {name:'Akshay'},{name:'AMit'}]
+
+let newObj={}
+array2.map((e)=>{
+let value = e.name.toUpperCase()
+newObj[value]=value})
+console.log(newObj,'<=newObj')
+
+const filterArray=array1.filter((i)=>{
+    let nameVal= i.name.toUpperCase()
+    console.log(newObj[nameVal],"jsdbjkds")
+    return newObj[nameVal]?i:null
+})
+console.log(filterArray,'<=FilteredAray')
